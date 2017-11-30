eb18daf1040f0099e42e572a07102be9859dd611
Dylan <dquesada186@gmail.com>
2017-11-30 07:47:55 -0600

    adds user function, total runability to ten digits

--- fibonacci.js
+++ fibonacci.js
@@ -54,16 +54,16 @@ function fibonacci(){
 	}
 }
 
-userFibonacci();
+runUserFibonacci();
 
-function userFibonacci(){
-	var fibonacciArray = [userInput()];
-	return fibonacciArray[0];
-}
 
 function runUserFibonacci(){
-	var fibonacciArray = userFibonacci();
-	for (var i = 0; i < 10; i++) {
-		[i]
+	var start = userInput("Enter your start number:");
+	var fibonacciArray = [];
+	fibonacciArray[0] = start;
+	fibonacciArray[1] = fibonacciArray[0] * 2;
+	for (var i = 2; i < 10; i++) {
+		fibonacciArray[i] = parseInt(fibonacciArray[i - 1]) + parseInt(fibonacciArray [i - 2]);
+		console.log(fibonacciArray);
 	}
 }
