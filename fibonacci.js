518693233a0ebbbe0ed6fdf8a48358e0b5ce5c05
Dylan <dquesada186@gmail.com>
2017-11-30 07:32:28 -0600

    adds a function that calculates fib. to 10 in array

--- fibonacci.js
+++ fibonacci.js
@@ -8,3 +8,50 @@ function userInput(message){
 		return prompt("Enter user input:");
 	}
 }
+
+// function createFibonacciString(){
+// 	var fibonacciArray = [];
+
+// 	var initialAdditor = 0;
+// 	for(var numberToPrint = 1; numberToPrint < 100; numberToPrint += placeHolder){
+// 		let placeHolder = placeHolder + numberToPrint;
+// 		console.log(numberToPrint);
+// 	}
+// }
+
+
+// function fibonacci(){
+// 	let leftMostNumber = 0;
+// 	let leftNumber = 1;
+// 	for (var i = 0; i < 15; i++) {
+// 		console.log("leftNumber " + leftNumber);
+// 		console.log("leftMostNumber " + leftMostNumber);
+		
+// 		leftNumber = leftNumber + leftMostNumber;
+// 		leftMostNumber = leftNumber;
+// 	}
+// }
+
+// function fibonacci(){
+// 	var fibonacciArray = ["1", "1", "1"];
+// 	let penultimateVariable = 1;
+// 	let lastVariable = 1;
+// 	for(var i = 2; i < 100; i++) {
+// 		lastVariable = fibonacciArray[i - 1];
+// 		penultimateVariable = fibonacciArray[i - 2];
+// 		fibonacciArray[i] = (lastVariable + penultimateVariable);
+// 	}
+// 	console.log(fibonacciArray);
+// }
+
+
+
+function fibonacci(){
+	var fibonacciArray = [0, 1];
+	for (var i = 2; i < 10; i++) {
+		fibonacciArray[i] = fibonacciArray[i - 1] + fibonacciArray [i - 2];
+		console.log(fibonacciArray);
+	}
+}
+
+fibonacci();
